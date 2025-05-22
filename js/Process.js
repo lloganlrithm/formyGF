ecee2fd21a7281be029880a8055657757f53513c
lloganlrithm <lloganlrithm@gmail.com>
2025-05-23 01:09:37 +0700

    Add files via upload

---
+++ js/Process.js
@@ -0,0 +1,3 @@
+document.getElementById('camera').addEventListener('click', () => window.location.href = 'page3.html');
+document.getElementById('message').addEventListener('click', () => window.location.href = 'page4.html');
+document.getElementById('song').addEventListener('click', () => window.location.href = 'page5.html');
